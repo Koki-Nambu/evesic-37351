90b03395187da3831a0e335f71b8e52a5797ff26
Koki-Nambu <ztailang414@gmail.com>
2022-01-27 16:31:20 +0900

    コントローラー定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,24 @@ class EventsController < ApplicationController
 
   def index
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def event_params
+    params.require(:event).permit(:image, :title, :date, :place, :charge, :content).merge(user_id: current_user.id)
+  end
 end
