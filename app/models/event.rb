dc00edc9bd39361dc1bc4a758f6f35f3c7da5eff
Koki-Nambu <ztailang414@gmail.com>
2022-01-27 14:04:22 +0900

    バリデーションの設定

---
+++ app/models/event.rb
@@ -0,0 +1,11 @@
+class Event < ApplicationRecord
+  is_impressionable counter_cache: true
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :charge, numericality: { only_integer: true, greater_than_or_equal_to: 100, less_than_or_equal_to: 30_000 },
+                      presence: { message: "Can't be blank" }
+  end
+
+  validates :title, :date, :place, :content, presence: true
+
+end
