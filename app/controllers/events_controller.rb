4522f1ed1dca3becdce6d047d5255a1fc4ed0b53
Koki-Nambu <ztailang414@gmail.com>
2022-01-31 19:03:26 +0900

    コントローラー定義

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,13 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_hot_event, only: [:index]
 
   def index
     @events = Event.all.order("created_at DESC")
+    @hot_event = @hot.limit(1).offset(0)
+    @hot_event2 = @hot.limit(1).offset(1)
+    @hot_event3 = @hot.limit(1).offset(2)
   end
 
   def new
@@ -19,7 +24,23 @@ class EventsController < ApplicationController
   end
 
   def show
-    @event = Event.find(params[:id])
+    impressionist(@event, nil, unique: [:ip_address])
+  end
+
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to event_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @event.destroy
+    redirect_to root_path
   end
 
 
@@ -28,4 +49,13 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:image, :title, :date, :place, :charge, :content).merge(user_id: current_user.id)
   end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  def set_hot_event
+    @hot = Event.order(impressions_count: 'DESC')
+  end
+
 end
