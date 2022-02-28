9a6c170bbbea0e5b516bbae5705595fe0fe6a675
Koki-Nambu <ztailang414@gmail.com>
2022-02-28 19:27:50 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root to: 'events#index'
   resources :users, only: [:show, :edit, :update]
   resources :events do
+    resources :comments, only: :create
     collection do
       get 'search'
     end
